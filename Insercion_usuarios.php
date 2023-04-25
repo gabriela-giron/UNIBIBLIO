08fa42d705d13ae492e94b9c2dd4a2a0e49047d0
Crisddl <131741324+Crisddl@users.noreply.github.com>
2023-04-25 00:30:11 -0500

    Update Insercion_usuarios.php

--- Insercion_usuarios.php
+++ Insercion_usuarios.php
@@ -8,6 +8,12 @@ include("funciones/conexion_bd.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="node_modules/sweetalert2/dist/sweetalert2.min.js"></script>
+    <link rel="stylesheet" href="node_modules/sweetalert2/dist/sweetalert2.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="responsive.css"/>
     <title>Insertar Usuarios</title>
 </head>
@@ -19,7 +25,7 @@ include("funciones/conexion_bd.php");
         <div class="titulo">
             <h1 align="center">Insertar Usuarios</h1>
         </div>
-        <div class="">
+        <div class="formulario">
             <form action="funciones/insertar_usuarios.php" method="post">
                 <section class="blocks">
                     <td>identificador </td>
@@ -90,41 +96,46 @@ include("funciones/conexion_bd.php");
             <div class="section_tabla">
                 <div class="fondo_tabla">
                     <div class="tabla">
-                        <?php
-                            // Mostrar tabla actualizada
-                            $consulta = "SELECT * FROM USUARIO";
-                            $resultado = mysqli_query($conn, $consulta);
-                            echo "<table border='1' align='center'>
-                                    <tr>
-                                        <th>ID Usuario</th>
-                                        <th>Nombre</th>
-                                        <th>Apellido</th>
-                                        <th>DPI</th>
-                                        <th>Telefono</th>
-                                        <th>Correo</th>
-                                        <th>Domicilio</th>
-                                        <th>Libros Prestados</th>
-                                        <th>Deuda</th>
-                                        <th>Estatus</th>
-                                        <th>Grupo</th>
-                                    </tr>";
-                            while ($fila = mysqli_fetch_array($resultado)) {
-                                echo "<tr>";
-                                echo "<td>" . $fila['ID_USUARIO'] . "</td>";
-                                echo "<td>" . $fila['NOMBRE'] . "</td>";
-                                echo "<td>" . $fila['APELLIDO'] . "</td>";
-                                echo "<td>" . $fila['DPI'] . "</td>";
-                                echo "<td>" . $fila['TELEFONO'] . "</td>";
-                                echo "<td>" . $fila['CORREO'] . "</td>";
-                                echo "<td>" . $fila['DOMICILIO'] . "</td>";
-                                echo "<td>" . $fila['LIBROS_PRESTADOS'] . "</td>";
-                                echo "<td>" . $fila['DEUDA'] . "</td>";
-                                echo "<td>" . $fila['ID_ESTATUS'] . "</td>";
-                                echo "<td>" . $fila['ID_GRUPO'] . "</td>";
-                                echo "</tr>";
-                            }
-                            echo "</table>";
-                        ?>
+                      
+	  <?php
+    		// Mostrar tabla actualizada con valores correspondientes de Estatus y Grupo
+    		$consulta = "SELECT u.ID_USUARIO, u.NOMBRE, u.APELLIDO, u.DPI, u.TELEFONO, u.CORREO, u.DOMICILIO, u.LIBROS_PRESTADOS, u.DEUDA, e.DESCRIPCION, g.DESCRIPCION as GDESCRIPCION
+                 FROM usuario u 
+                 JOIN estatus e ON u.ID_ESTATUS = e.ID_ESTATUS 
+                 JOIN grupo g ON u.ID_GRUPO = g.ID_GRUPO";
+    $resultado = mysqli_query($conn, $consulta);
+    echo "<table border='1' align='center'>
+            <tr>
+                <th>ID Usuario</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>DPI</th>
+                <th>Telefono</th>
+                <th>Correo</th>
+                <th>Domicilio</th>
+                <th>Libros Prestados</th>
+                <th>Deuda</th>
+                <th>Estatus</th>
+                <th>Grupo</th>
+            </tr>";
+    while ($fila = mysqli_fetch_array($resultado)) {
+        echo "<tr>";
+        echo "<td>" . $fila['ID_USUARIO'] . "</td>";
+        echo "<td>" . $fila['NOMBRE'] . "</td>";
+        echo "<td>" . $fila['APELLIDO'] . "</td>";
+        echo "<td>" . $fila['DPI'] . "</td>";
+        echo "<td>" . $fila['TELEFONO'] . "</td>";
+        echo "<td>" . $fila['CORREO'] . "</td>";
+        echo "<td>" . $fila['DOMICILIO'] . "</td>";
+        echo "<td>" . $fila['LIBROS_PRESTADOS'] . "</td>";
+        echo "<td>" . $fila['DEUDA'] . "</td>";
+        echo "<td>" . $fila['DESCRIPCION'] . "</td>";
+        echo "<td>" . $fila['GDESCRIPCION'] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+?>
+
                     </div>
                 </div> 
             </div>
