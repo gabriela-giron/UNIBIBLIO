53fbfb15a4483b979e8a843de53d872e6db7551d
Crisddl <131741324+Crisddl@users.noreply.github.com>
2023-04-25 00:30:53 -0500

    Update insertar_usuarios.php

--- funciones/insertar_usuarios.php
+++ funciones/insertar_usuarios.php
@@ -1,3 +1,11 @@
+<html>
+    <head>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10.16.6/dist/sweetalert2.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.16.6/dist/sweetalert2.min.js"></script>
+	<meta http-equiv="refresh" content="2;url=http://localhost/UNIBIBLIO/insercion_usuarios.php">
+
+<body>
+
 <?php
 
 include("conexion_bd.php"); 
@@ -15,29 +23,40 @@ if (isset($_POST['btn-ingresar'])){
     $estatus=$_POST['id_estatus'];
     $grupo=$_POST['id_grupo'];
 
-    // Verificar si el usuario ya existe en la base de datos
+     // Verificar si el usuario ya existe en la base de datos
     $busqueda="SELECT * FROM USUARIO WHERE ID_USUARIO=".$id."";
     $exec=mysqli_query($conn,$busqueda);
     $contenido=mysqli_fetch_array($exec);
 
-    if (is_array($contenido)) {
-        echo "<script>alert('Error Ya existe el codigo');</script>";
-    } else {
-        if($grupo==0){
-            echo "<script>alert('No se selecciono grupo');</script>";   
-        }else{
-            $consulta="INSERT INTO USUARIO VALUES(".$id.",'".$nombre."','".$apellido."',".$dpi.",".$telefono.",'".$correo."','".$domicilio."',".$libros_prestados.",".$deuda.",".$estatus.",".$grupo.")";
-            //$consulta2="SELECT * FROM BD_UNIBIBLIO.LIBROS";
-        }
-        $exec=mysqli_query($conn,$consulta);
-        if (!$exec) {
-            echo "<script>alert('Error al ingresar al usuario');</script>";
-        }else{
-            /*$consulta2 = "SELECT * FROM BD_UNIBIBLIO.LISTA_USUARIOS";
-            $exec2=mysqli_query($conn,$consulta2);*/
-            echo "<script>alert('Usuario ingresado con exito');</script>";
-        }
-    }
+if (is_array($contenido)) {
+  echo '<script>swal.fire("Error", "Ya existe el código", "error").then(() => {
+    window.location.href = "../Insercion_usuarios.php";
+  });</script>';
+} else {
+  if($grupo==0){
+    echo '<script>swal.fire("Error", "No se seleccionó grupo", "error").then(() => {
+      window.location.href = "../Insercion_usuarios.php";
+    });</script>';
+  }else{
+    $consulta="INSERT INTO USUARIO VALUES(".$id.",'".$nombre."','".$apellido."',".$dpi.",".$telefono.",'".$correo."','".$domicilio."',".$libros_prestados.",".$deuda.",".$estatus.",".$grupo.")";
+  }
+  $exec=mysqli_query($conn,$consulta);
+  if(!$exec){
+    echo '<script>swal.fire("Error", "Error al ingresar el usuario", "error").then(() => {
+      window.location.href = "../Insercion_usuarios.php";
+    });</script>';
+  }else{
+    echo '<script>swal.fire("Éxito", "Usuario ingresado con éxito", "success").then(() => {
+      window.location.href = "../Insercion_usuarios.php";
+    });</script>';
+  }
+}
 }
 
 ?>
+
+</body>
+
+    </head>
+</html>
+
